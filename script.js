d0337e3c3879b845184b71a898b040b644bfe1be
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-10-20 17:38:49 +0200

    add buttons in html and css and javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let borderColor = '#000000';
+
+const redButton = document.getElementById("red-button");
+const blueButton = document.getElementById("blue-button");
+const greenButton = document.getElementById("green-button");
+
