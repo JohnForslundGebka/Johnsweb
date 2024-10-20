f597f44d7d6a565a026146d898831f19e67e25a3
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-10-20 17:57:23 +0200

    add javascsript conditioning to buttons

--- script.js
+++ script.js
@@ -5,3 +5,53 @@ const redButton = document.getElementById("red-button");
 const blueButton = document.getElementById("blue-button");
 const greenButton = document.getElementById("green-button");
 
+const gridItems = document.querySelectorAll(".grid-item");
+
+//add hoover effect to all grid items
+function updateHoverEffect(){
+    gridItems.forEach(item => {
+        item.addEventListener("mouseover", function(){
+            item.style.borderColor = borderColor;
+        });
+
+        item.addEventListener("mouseout", function(){
+            item.style.borderColor = "transparent";
+        });
+    });
+}
+
+
+redButton.addEventListener("click", function() {
+    if(borderColor != 'red'){  
+        borderColor = "red";
+    } else{
+        borderColor = '#000000';
+    }
+    
+    updateHoverEffect();
+});
+
+blueButton.addEventListener("click", function() {
+    if(borderColor != 'blue'){  
+        borderColor = "blue";
+    } else{
+        borderColor = '#000000';
+    }
+
+    updateHoverEffect();
+});
+
+greenButton.addEventListener("click", function() {
+    if(borderColor != 'green'){  
+        borderColor = "green";
+    } else{
+        borderColor = '#000000';
+    }
+
+    updateHoverEffect();
+});
+
+ 
+
+
+updateHoverEffect();
